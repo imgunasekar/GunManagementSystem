fa581581f8b1052006e56057f0dd0d873b3bc11b
Gunasekar K <gunasekar08.k@gmail.com>
2024-07-15 19:31:25 +0530

    Array version added

--- main.cpp
+++ main.cpp
@@ -15,16 +15,21 @@ int main()
 	Pistol pistol("Glock", "9mm", false, true);
 
 	/* Testcase 2 - Create array of base pointer and store concrete class objects into it */
-	vector<Gun*> guns;
-	guns.push_back(&revolver);
-	guns.push_back(&pistol);
+	vector<Gun*> gunCabinet;
+	gunCabinet.push_back(&revolver);
+	gunCabinet.push_back(&pistol);
 
 	/* Testcase 3 - Iterator over dynamic array (vector object created in testcase 2) */
-	vector<Gun*>::iterator it = guns.begin();
-	for (;it != guns.end(); ++it)
-	{
+	cout << "My Gun Cabinet Contains:" << endl;
+	vector<Gun*>::iterator it = gunCabinet.begin();
+	for (;it != gunCabinet.end(); ++it)
 		(*it)->print();
-	}
+
+	/* Array of base class objects */
+	//Gun* gunCabinet[] = { &revolver, &pistol };
+	//int numGuns = sizeof(gunCabinet) / sizeof(gunCabinet[0]);
+	//for (int i = 0; i < numGuns; i++)
+		//gunCabinet[i]->print();
 
 	return 0;
 }
